056afcdb2fdfe82d8fe0dd1496d04f21182398d6
Ilya Matveev <ilya.matveev@jetbrains.com>
2021-01-22 13:46:27 +0700

    [New MM] Support thread switching in codegen

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/CAdapterGenerator.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/CAdapterGenerator.kt
@@ -222,7 +222,7 @@ private class ExportedElement(val kind: ElementKind,
                         val numParams = LLVMCountParams(llvmFunction)
                         val args = (0 .. numParams - 1).map { index -> param(index) }
                         val callee = lookupVirtualImpl(receiver, irFunction)
-                        val result = call(callee, args, exceptionHandler = ExceptionHandler.Caller, verbatim = true)
+                        val result = call(callee, args, exceptionHandler = ExceptionHandler.Caller(), verbatim = true)
                         ret(result)
                     }
                 } else {
@@ -243,7 +243,7 @@ private class ExportedElement(val kind: ElementKind,
                 // Produce instance getter if needed.
                 if (isSingletonObject) {
                     generateFunction(owner.codegen, owner.kGetObjectFuncType, "${cname}_instance") {
-                        val value = getObjectValue(irClass, ExceptionHandler.Caller, null)
+                        val value = getObjectValue(irClass, ExceptionHandler.Caller(), null)
                         ret(value)
                     }
                 }
@@ -253,7 +253,7 @@ private class ExportedElement(val kind: ElementKind,
                 cname = "_konan_function_${owner.nextFunctionIndex()}"
                 generateFunction(owner.codegen, owner.kGetObjectFuncType, cname) {
                     val irEnumEntry = irSymbol.owner as IrEnumEntry
-                    val value = getEnumEntry(irEnumEntry, ExceptionHandler.Caller)
+                    val value = getEnumEntry(irEnumEntry, ExceptionHandler.Caller())
                     ret(value)
                 }
             }
